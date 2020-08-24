ac7363dcb15d9f974ee919ef58926007738b120a
Matthias Perenthaler <matthias.perenthaler@gym-wst.eu>
2020-08-24 13:36:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var button;
+
+function setup() {
+  createCanvas(400, 300);
+  background(240);
+
+  button = createButton("Plopp!");
+  button.position(20, 20);
+  button.mousePressed(plopp);
+}
+
+function draw() {
+}
+
+function plopp() {
+  fill(random(255), random(255), random(255));
+  ellipse(random(width), random(height), random(10,60));
+}
